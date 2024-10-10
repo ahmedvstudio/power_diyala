b71eaf81071e6edc17835e3ffc03d989e2093f28
RedJohn <ahmed47v@gmail.com>
2024-10-11 02:07:44 +0300

    updated

--- lib/screens/stepper.dart
+++ lib/screens/stepper.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:power_diyala/widgets/dateformat.dart';
+import 'package:power_diyala/widgets/detect_date_format.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:power_diyala/data_helper/database_helper.dart';
 import 'package:power_diyala/main.dart';
