ffce7135e47aa51fa530be6f0e7a10bc957212eb
RedJohn <ahmed47v@gmail.com>
2024-10-30 14:02:06 +0300

    added

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,11 +1,15 @@
+import 'dart:async';
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:power_diyala/screens/calc_screen.dart';
-import 'package:power_diyala/screens/teams_screen.dart';
-import 'package:power_diyala/screens/spms_screen.dart';
 import 'package:power_diyala/screens/network_screen.dart';
+import 'package:power_diyala/screens/pm_sheet_screen.dart';
 import 'package:power_diyala/screens/setting_screen.dart';
+import 'package:power_diyala/screens/spms_screen.dart';
+import 'package:power_diyala/screens/teams_screen.dart';
 import 'package:power_diyala/settings/theme_control.dart';
+import 'package:provider/provider.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -14,13 +18,35 @@ class MainScreen extends StatefulWidget {
   MainScreenState createState() => MainScreenState();
 }
 
-class MainScreenState extends State<MainScreen> {
+class MainScreenState extends State<MainScreen> with TickerProviderStateMixin {
+  final autoSizeGroup = AutoSizeGroup();
+// Default index of the first screen
   int _selectedIndex = 0;
   final List<Widget> _screens = [];
+  late AnimationController _fabAnimationController;
+  late AnimationController _borderRadiusAnimationController;
+  late Animation<double> fabAnimation;
+  late Animation<double> borderRadiusAnimation;
+  late CurvedAnimation fabCurve;
+  late CurvedAnimation borderRadiusCurve;
+  final List<String> labels = [
+    "Calculator", // For index 0
+    "Teams", // For index 1
+    "SPMS", // For index 2
+    "Network", // For index 3
+  ];
+
+  final iconList = <IconData>[
+    Icons.calculate_rounded,
+    Icons.group_rounded,
+    Icons.home_repair_service_rounded,
+    Icons.webhook_rounded,
+  ];
 
   @override
   void initState() {
     super.initState();
+// Initialize screens
     _screens.add(
       Consumer<ThemeControl>(
         builder: (context, themeControl, child) {
@@ -45,6 +71,7 @@ class MainScreenState extends State<MainScreen> {
         },
       ),
     );
+
     _screens.add(
       Consumer<ThemeControl>(
         builder: (context, themeControl, child) {
@@ -69,17 +96,36 @@ class MainScreenState extends State<MainScreen> {
         },
       ),
     );
-    _screens.add(
-      Consumer<ThemeControl>(
-        builder: (context, themeControl, child) {
-          return SettingsScreen(
-            themeMode: themeControl.themeMode,
-            onThemeChanged: (value) {
-              themeControl.toggleTheme(value);
-            },
-          );
-        },
-      ),
+
+    _fabAnimationController = AnimationController(
+      duration: Duration(milliseconds: 500),
+      vsync: this,
+    );
+    _borderRadiusAnimationController = AnimationController(
+      duration: Duration(milliseconds: 500),
+      vsync: this,
+    );
+    fabCurve = CurvedAnimation(
+      parent: _fabAnimationController,
+      curve: Interval(0.5, 1.0, curve: Curves.fastOutSlowIn),
+    );
+    borderRadiusCurve = CurvedAnimation(
+      parent: _borderRadiusAnimationController,
+      curve: Interval(0.5, 1.0, curve: Curves.fastOutSlowIn),
+    );
+
+    fabAnimation = Tween<double>(begin: 0, end: 1).animate(fabCurve);
+    borderRadiusAnimation = Tween<double>(begin: 0, end: 1).animate(
+      borderRadiusCurve,
+    );
+
+    Future.delayed(
+      Duration(seconds: 1),
+      () => _fabAnimationController.forward(),
+    );
+    Future.delayed(
+      Duration(seconds: 1),
+      () => _borderRadiusAnimationController.forward(),
     );
   }
 
@@ -93,6 +139,7 @@ class MainScreenState extends State<MainScreen> {
   Widget build(BuildContext context) {
     final themeControl = Provider.of<ThemeControl>(context);
     return Scaffold(
+      extendBody: true,
       appBar: AppBar(
         title: Text(
           'Power Diyala',
@@ -101,55 +148,88 @@ class MainScreenState extends State<MainScreen> {
         actions: [
           IconButton(
             onPressed: () {
-              Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (context) => _screens[4]));
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => PmSheetPage(
+                        themeMode: themeControl.themeMode,
+                        onThemeChanged: (value) {
+                          themeControl.toggleTheme(value);
+                        },
+                      )));
+            },
+            icon: const Icon(Icons.add),
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => SettingsScreen(
+                        themeMode: themeControl.themeMode,
+                        onThemeChanged: (value) {
+                          themeControl.toggleTheme(value);
+                        },
+                      )));
             },
             icon: const Icon(Icons.engineering),
             tooltip: 'Settings',
           ),
         ],
       ),
-      body: _screens[_selectedIndex],
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          color: Theme.of(context).scaffoldBackgroundColor,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.1),
-              blurRadius: 8.0,
-              offset: const Offset(0, -2),
-            ),
-          ],
-          borderRadius: const BorderRadius.vertical(top: Radius.circular(16)),
-        ),
-        child: ClipRRect(
-          borderRadius: const BorderRadius.vertical(top: Radius.circular(16)),
-          child: BottomNavigationBar(
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.calculate),
-                label: 'Calculator',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.groups_sharp),
-                label: 'Teams',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home_repair_service),
-                label: 'SPMS',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.webhook),
-                label: 'Network',
-              ),
-            ],
-            currentIndex: _selectedIndex,
-            unselectedItemColor: themeControl.secondaryColor,
-            selectedItemColor: themeControl.primaryColor,
-            showUnselectedLabels: true,
-            onTap: _onItemTapped,
-            type: BottomNavigationBarType.shifting,
+      body: NotificationListener<ScrollNotification>(
+        child: _screens[_selectedIndex],
+      ),
+      floatingActionButton: FadeTransition(
+        opacity: fabAnimation,
+        child: FloatingActionButton(
+          shape: CircleBorder(),
+          child: Icon(
+            Icons.add,
           ),
+          onPressed: () {},
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: AnimatedBottomNavigationBar.builder(
+        itemCount: iconList.length,
+        tabBuilder: (int index, bool isActive) {
+          return Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(iconList[index],
+                  size: 20,
+                  color: isActive
+                      ? Theme.of(context).primaryColor
+                      : Theme.of(context).secondaryHeaderColor),
+              const SizedBox(height: 4),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 4),
+                child: AutoSizeText(
+                  labels[index],
+                  maxLines: 1,
+                  style: TextStyle(
+                      color: isActive
+                          ? Theme.of(context).primaryColor
+                          : Theme.of(context).secondaryHeaderColor),
+                  group: autoSizeGroup,
+                ),
+              )
+            ],
+          );
+        },
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        activeIndex: _selectedIndex,
+        splashColor: Theme.of(context).colorScheme.tertiary,
+        notchAndCornersAnimation: borderRadiusAnimation,
+        splashSpeedInMilliseconds: 300,
+        notchSmoothness: NotchSmoothness.smoothEdge,
+        gapLocation: GapLocation.center,
+        leftCornerRadius: 32,
+        rightCornerRadius: 32,
+        onTap: _onItemTapped,
+        shadow: BoxShadow(
+          offset: Offset(0, 1),
+          blurRadius: 12,
+          spreadRadius: 0.5,
+          color: Theme.of(context).shadowColor,
         ),
       ),
     );
